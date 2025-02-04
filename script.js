9434c6ec3189e6c821f8eceaa3b13d662da2b8d4
Harjot <hajotsingh2552@gmail.com>
2025-02-03 19:11:31 -0600

    Set the initial turn to x and changing it to O if x already taken the turn and vice versa

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let music = new Audio("music.mp3")
 let turnMusic = new Audio("ting.mp3")
 let gameOver = new Audio("gameover.mp3");
 let gameover= false;
+
+
+let turn = "X";
+
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+}
