742562f742c8dca422610f6ffceed7f8ba6db10c
Harjot <hajotsingh2552@gmail.com>
2025-02-03 19:13:02 -0600

    Stored all the wins conditions in an array and mapping it if the x or o has occupied that space, if yes then setting the gameover = true

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ let turn = "X";
 const changeTurn = () => {
     return turn === "X" ? "0" : "X";
 }
+
+
+const checkWin = () => {
+    let boxTexts = document.getElementsByClassName("boxText")
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+
+    wins.forEach(e=>{
+        if((boxTexts[e[0]].innerText === boxTexts[e[1]].innerText && boxTexts[e[2]].innerText === boxTexts[e[1]].innerText ) && boxTexts[e[0]].innerText !== ""){
+            document.querySelector(".info").innerText = boxTexts[e[0]].innerText + " Won";
+            gameover = true;
+            document.querySelector(".imgBox").getElementsByTagName("img")[0].style.width = "200px"
+        }
+    })
+}
